6d9fb0f0f27325db6feb7aa92e0e184575b5382a
Mo Weiss <mshwss@gmail.com>
2019-01-16 11:12:14 -0500

    Initial Commit

---
+++ GuessingGame1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessingGame1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
